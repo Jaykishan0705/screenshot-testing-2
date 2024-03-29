ef64f80c9fa7c866e17a2b5ef11221c8c5fe293f
Devansh Aryan <devansh.aryan@sprinklr.com>
2024-03-29 17:15:32 +0530

    fixed styles

--- src/components/VRTDisplay.tsx
+++ src/components/VRTDisplay.tsx
@@ -36,47 +36,51 @@ export const VRTDisplay = (): ReactElement => {
 
   return (
     <>
-      <Box className="p-4 flex border-1 spr-focus-01 border-solid">
-        <Box className="w-2/24">
+      <Box className="p-4 flex border-1 border-solid spr-focus-01 gap-4">
+        <Box className="flex flex-col w-2/24 gap-6">
+        <Box className="flex justify-center text-11 font-bold">Path</Box>
           {imgArr
             .filter((obj: ImageType) => shouldDisplay(obj.showType, showType))
             .map((obj: ImageType, imageNo: number) => (
-              <Box className="p-4 h-64 text-11 font-bold border-1 spr-border-04" key={obj.alt}>
+              <Box className="px-4 flex justify-center items-center h-64 text-11 font-bold" style={{width:"100%",height:"80vh"}} key={obj.alt}>
                 {imageNo + 1}. {obj.alt}
               </Box>
             ))}
         </Box>
 
-        <Box className="w-6/24">
+        <Box className="flex flex-col w-6/24 gap-6">
+        <Box className="flex justify-center text-11 font-bold">Baseline</Box>
           {imgArr
             .filter((obj: ImageType) => shouldDisplay(obj.showType, showType))
             .map((obj: ImageType) => (
-              <Box onClick={() => openPopUp(obj.srcBase)} className="pt-3 h-64 border-1 spr-border-04" key={obj.alt}>
-                <Image src={obj.srcBase} alt={obj.alt} className={{ height: '230px', margin: 'auto' }} />
+              <Box onClick={() => openPopUp(obj.srcBase)} className="overflow-y-hidden hover:cursor-pointer" style={{width:"100%",height:"80vh"}} key={obj.alt}>
+                <Image src={obj.srcBase} alt={obj.alt} className="object-contain h-full w-full" />
               </Box>
             ))}
         </Box>
 
-        <Box className="w-6/24">
+        <Box className="flex flex-col w-6/24 gap-6">
+        <Box className="flex justify-center text-11 font-bold">Current</Box>
           {imgArr
             .filter((obj: ImageType) => shouldDisplay(obj.showType, showType))
             .map((obj: ImageType) => (
-              <Box onClick={() => openPopUp(obj.srcCur)} className="pt-3 h-64 border-1 spr-border-04" key={obj.alt}>
-                <Image src={obj.srcCur} alt={obj.alt} className={{ height: '230px', margin: 'auto' }} />
+              <Box onClick={() => openPopUp(obj.srcCur)} key={obj.alt}className="overflow-y-hidden hover:cursor-pointer" style={{width:"100%",height:"80vh"}}>
+                <Image src={obj.srcCur} alt={obj.alt} className="object-contain h-full w-full"/>
               </Box>
             ))}
         </Box>
 
-        <Box className="w-6/24">
+        <Box className="flex flex-col w-6/24 gap-6">
+        <Box className="flex justify-center text-11 font-bold">Difference</Box>
           {imgArr
             .filter((obj: ImageType) => shouldDisplay(obj.showType, showType))
             .map((obj: ImageType) => {
               if (obj.showType !== 'changed') {
-                return <Box className="h-64 border-1 spr-border-04" />;
+                return <Box style={{width:"100%",height:"80vh"}}/>;
               } else {
                 return (
-                  <Box onClick={() => openPopUp(obj.srcDiff)} className="pt-3 h-64 border-1 spr-border-04">
-                    <Image src={obj.srcDiff} alt={obj.alt} className={{ height: '230px', margin: 'auto' }} />
+                  <Box onClick={() => openPopUp(obj.srcDiff)} key={obj.alt} className="overflow-y-hidden hover:cursor-pointer" style={{width:"100%",height:"80vh"}}>
+                    <Image src={obj.srcDiff} alt={obj.alt} className="object-contain h-full w-full " />
                   </Box>
                 );
               }

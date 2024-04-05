06cd09a60b8c4f5ada7064a5938ef5acca484796
Jaykishan Parmar <jaykishan.parmar@sprinklr.com>
2024-04-05 16:17:13 +0530

    [SPR-00] fix: temp

--- pages/api/get-images.js
+++ pages/api/get-images.js
@@ -1,8 +1,21 @@
 import fs from 'fs';
+import { exec as execCallback } from 'child_process';
+
+const exec = (command: string) =>
+  new Promise(resolve => {
+    execCallback(command, (err, stdout) => {
+      console.log(stdout);
+      console.error('err:', err);
+      return resolve(stdout);
+    });
+  });
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     const { filename } = req.query; // Get filename from query parameter
+
+    console.log('__dir', __dirname, __filename);
+    await exec('ls');
 
     const imagePath1 = `pages/api/database/.lostpixel/baseline/pricing-consumer-intelligence.png`;
 

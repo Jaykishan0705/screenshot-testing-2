99b32691d06666dbac4cac92ceb4ceaefbf4fb45
Jaykishan Parmar <jaykishan.parmar@sprinklr.com>
2024-04-25 12:44:32 +0530

    [CL-00] fix: temp

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -14,30 +14,4 @@ export const config = {
   generateOnly: true,
   failOnDifference: false,
   compareEngine: 'odiff',
-  beforeScreenshot: async (page: any) => {
-    const scrollPage = async (args: any) => {
-      const { direction, speed } = args;
-      const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-      const scrollHeight = () => document.body.scrollHeight;
-      const start = direction === 'down' ? 0 : scrollHeight();
-      const shouldStop = (position: number) => (direction === 'down' ? position > scrollHeight() : position < 0);
-      const increment = direction === 'down' ? 100 : -100;
-      const delayTime = speed === 'slow' ? 50 : 10;
-
-      let i = start;
-
-      const scrollStep = async () => {
-        if (!shouldStop(i)) {
-          window.scrollTo(0, i);
-          i += increment;
-          await delay(delayTime);
-          await scrollStep();
-        }
-      };
-
-      await scrollStep();
-    };
-    await page.evaluate(scrollPage, { direction: 'down', speed: 'slow' });
-    await page.evaluate(scrollPage, { direction: 'up', speed: 'fast' });
-  },
 };
--- pages/api/initiateCompare.ts
+++ pages/api/initiateCompare.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { exec as execCallback } from 'child_process';
 import { v4 as uuidv4 } from 'uuid';
+import { runner } from 'lost-pixel/dist/runner'
 
 import tasks from '@/src/tasks';
 
@@ -11,6 +12,24 @@ const exec = (command: string) =>
       return resolve(stdout);
     });
   });
+
+const config = {
+  pageShots: {
+    pages: [
+      { path: '/pricing/customer-service/', name: 'pricing-customer-service' },
+      { path: '/pricing/social-media-management/', name: 'pricing-social-media-management' },
+      { path: '/pricing/consumer-intelligence', name: 'pricing-consumer-intelligence' },
+      { path: '/pricing/marketing-and-advertising', name: 'pricing-marketing-and-advertising' },
+    ],
+    baseUrl: process.env.TARGET_URL || 'https://www.sprinklr.com',
+  },
+  imagePathBaseline: 'pages/api/database/.lostpixel/baseline',
+  imagePathCurrent: 'pages/api/database/.lostpixel/current',
+  imagePathDifference: 'pages/api/database/.lostpixel/difference',
+  generateOnly: true,
+  failOnDifference: false,
+  compareEngine: 'odiff',
+};
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { baseUrl, compareUrl } = req.body;
@@ -26,7 +45,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   process.nextTick(async () => {
     try {
       process.env.TARGET_URL = baseUrl;
-      await exec('yarn lost-pixel update');
+      await runner(config);
 
       process.env.TARGET_URL = compareUrl;
       await exec('yarn lost-pixel');
